e39fa01896168f95df8dc908741bc38a1671c81c
timmyuO <111019271+timmyuO@users.noreply.github.com>
2022-08-20 18:02:43 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using EventfulAssignment;
+using System;
+
+namespace DelegatesAndEvents
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Notifier notifier = new Notifier();
+            notifier.ActOnMultipleLogin();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventfulAssignment
+{
+
+    internal class AccountAccess //publisher
+    {
+
+        public delegate void NotifyHandler();
+        public event NotifyHandler NotifyOtherClasses;
+
+        public void MultipleLogin()
+        {
+            Console.WriteLine("Login on another device in progress");
+            System.Threading.Thread.Sleep(10000);
+            //when Multiple login is detected
+            OnMultipleLoginDetected();
+        }
+        protected virtual void OnMultipleLoginDetected()
+        {
+            NotifyOtherClasses?.Invoke();  //raise event
+        }
+
+    }
+
+    public class Notifier  //subscriber
+    {
+        public void ActOnMultipleLogin()
+        {
+            var accAcc = new AccountAccess();
+            accAcc.NotifyOtherClasses += LoginDetectorActions;
+            accAcc.NotifyOtherClasses += () => Console.WriteLine("Send a notification to email to verify user");
+
+            accAcc.MultipleLogin();
+
+        }
+
+        public void LoginDetectorActions()
+        {
+            Console.WriteLine("Send a notification to email to verify user");
+            Console.ReadLine();
+        }
+    }
+}
+
